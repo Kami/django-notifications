d2ca9b0feef2de85c07d2a3256fe8b09680097cf
Tomaž Muraus <kami@k5-storitve.net>
2010-06-04 17:02:46 +0200

    Some refactoring of the management commands + added colors

--- django_notifications/management/commands/notifications.py
+++ django_notifications/management/commands/notifications.py
@@ -10,38 +10,43 @@ Usage:
 	notification backends
 	
 	python manage.py notifications settings [backend_key] - displays the settings for
-	the specified backend.
+	the specified backend
 """
 
 from django.core.management.base import BaseCommand, CommandError
 
 from django_notifications import backends
+from django_notifications.management.commands.color import style
+
+VALID_COMMANDS = {
+	'backends': (0, None),
+	'configured_backends': (0, None),
+	'settings': (1, '[backend name]')
+}
 
 
 class Command(BaseCommand):
 	help = 'Manage notifications'
 	args = 'subcommand'
 	requires_model_validation = False
-	
-	def handle(self, *args, **kwargs):
-		valid_commands = ('backends', 'configured_backends', 'settings')
-		
+
+	def handle(self, *args, **kwargs):		
 		if len(args) == 0:
 			raise CommandError('You need to specify a subcommand: %s' % \
 							(', ' . join(valid_commands)))
-		elif len(args) > 1 and args[0] != 'settings':
-			raise CommandError('Invalid number of arguments: %s' % \
-							(', ' . join(args)))
 		else:
 			sub_command = args[0]
 			
-			if not sub_command in valid_commands:
+			if not sub_command in VALID_COMMANDS.keys():
 				raise CommandError('Invalid subcommand specified. Valid choices are: %s' % \
-							(', ' . join(valid_commands)))
-				
-			if sub_command == 'settings' and len(args) != 2:
-				raise CommandError('Invalid number of arguments. Usage %s [backend_key]' % \
-							(', ' . join(args)))
+							(', ' . join(VALID_COMMANDS.keys())))
+			
+			argument_count = VALID_COMMANDS[sub_command][0]
+			argument_help = VALID_COMMANDS[sub_command][1]
+			
+			if argument_count != len(args) - 1:
+				raise CommandError('Invalid number of arguments. Usage: %s %s' % \
+							(sub_command, argument_help if argument_help else ''))
 				
 			command = getattr(self, 'CMD_%s' % (sub_command))
 			command(*args[1:])
@@ -49,10 +54,10 @@ class Command(BaseCommand):
 	def CMD_backends(self):
 		try:
 			available_backends = backends.get_available_backends()
-			
-			print 'Available backends:'
+
+			print style.GREEN('Available backends:')
 			for (key, value, description) in available_backends:
-				print '%s - %s (%s)' % (key, value, description)
+				print '- ', style.BOLD('%s' % (key)), '- %s (%s)' % (value, description)
 		except KeyboardInterrupt:
 			pass
 				
@@ -60,9 +65,9 @@ class Command(BaseCommand):
 		try:
 			configured_backends = backends.get_available_backends(configured_only = True)
 			
-			print 'Available and configured backends:'
-			for (key, value, _) in configured_backends:
-				print '%s - %s' % (key, value)
+			print style.GREEN('Available and configured backends:')
+			for (key, value, description) in configured_backends:
+				print '- ', style.BOLD('%s' % (key)), '- %s (%s)' % (value, description)
 		except KeyboardInterrupt:
 			pass
 		
@@ -70,9 +75,9 @@ class Command(BaseCommand):
 		try:
 			backend_settings = backends.get_settings(backend_key)
 
-			print '%s backend settings:' % (backend_key)
+			print style.GREEN('Settings for "%s" backend:' % (backend_key))
 			for (key, value) in backend_settings.iteritems():
-				print '%s:\t\t %s' % (key, value)
+				print style.BOLD('%s:' % (key)), '\t\t %s' % (value)
 		except EnvironmentError:
 			print 'Invalid backend key'
 		except KeyboardInterrupt:
